17652b12c2e42c3781caf3d9f7daa70b0cc7c285
july-12 <seventanyb@gmail.com>
2023-03-05 12:36:12 +0800

    rename: views pages

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import { getToken } from '@/utils/token'
+import request from '@/utils/request'
+
+const HomePage = () => {
+  const location = useLocation()
+  console.log(location)
+
+  const loaderData = async () => {
+    const token = getToken()
+    if(token) {
+        const res: any = await request.get('/api/user_info')
+        console.log(res)
+    }
+  }
+  useEffect(() => {
+    loaderData()
+  }, [])
+  return <div>this is home page</div>
+}
+
+export default HomePage
