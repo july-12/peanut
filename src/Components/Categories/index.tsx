17b29beb2817b64bf6d1596eb3574f2c1c7ac915
july-12 <seventanyb@gmail.com>
2023-03-10 23:12:38 +0800

    feat: categories

--- src/Components/Categories/index.tsx
+++ src/Components/Categories/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { map } from 'lodash'
+import { useStore } from '@/store'
+import { Tag } from 'antd'
 
-import "./index.scss"
+import './index.scss'
 
 const Categories = () => {
-  return <div className='categories'>Categories</div>
+  const {
+    tag: { list }
+  } = useStore('tag')
+  return (
+    <div className="categories">
+      <span className="categories-label">标签: </span>
+      {map(list, (tag) => (
+        <div className="category-item">
+          <Tag key={tag.id} color={tag.color}>
+            {tag.name}
+          </Tag>
+          <div className="count">{tag.posts.length}</div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Categories
--- src/models/tag.ts
+++ src/models/tag.ts
@@ -1,12 +1,16 @@
-import { createModel } from "@rematch/core";
+import { createModel } from '@rematch/core'
 import request from '@/utils/request'
-import { RootModel } from ".";
+import { RootModel } from '.'
 import * as api from '@/api'
 
 export interface ITag {
-    id: number
-    color?: string
-    name: string
+  id: number
+  color?: string
+  name: string
+}
+interface IPost {
+  id: number
+  title: string
 }
 export interface ITagValue {
   name: string
@@ -14,8 +18,8 @@ export interface ITagValue {
 }
 
 interface ITagState {
-    list: ITag[]
-    postList: ITag[]
+  list: (ITag & { posts: IPost[] })[]
+  postList: ITag[]
 }
 
 export const tag = createModel<RootModel>()({
@@ -25,25 +29,25 @@ export const tag = createModel<RootModel>()({
   } as ITagState,
   reducers: {
     update(state, payload: Partial<ITagState>) {
-      return { ...state, ...payload};
-    },
+      return { ...state, ...payload }
+    }
   },
   effects: (dispatch) => ({
     async getTags(params: any) {
-        const res = await api.tags.list<ITag[]>(params)
-        dispatch.tag.update({ list: res })
+      const res = await api.tags.list<ITag[]>(params)
+      dispatch.tag.update({ list: res })
     },
     async createTag(value: ITagValue) {
-        await api.tags.create<ITagValue>(value)
+      await api.tags.create<ITagValue>(value)
     },
     async createTags(value: { tags: ITagValue[] }) {
-        const res = await request.post(`${api.baseURL}/tags/batch`, value).then((res) => res.data)
+      const res = await request.post(`${api.baseURL}/tags/batch`, value).then((res) => res.data)
     },
     async updateTag(value: ITag) {
-        await api.tags.update<ITag>(value.id, value)
+      await api.tags.update<ITag>(value.id, value)
     },
     async deleteTag(id: number) {
-        await api.tags.remove(id)
-    },
-  }),
-});
+      await api.tags.remove(id)
+    }
+  })
+})
